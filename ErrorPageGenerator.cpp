3249cd2847201f92b953be76c30c8938cafdc9f2
Miguel Bragança <mipinhei@student.42porto.com>
2026-03-07 13:26:36 +0000

    Update ErrorPageGenerator.cpp

---
+++ ErrorPageGenerator.cpp
@@ -0,0 +1,7 @@
+#include "ErrorPageGenerator.hpp"
+
+std::string ErrorPageGenerator::generateErrorPage(int statusCode, const std::string& message){
+}
+
+    std::string ErrorPageGenerator::loadCustomErrorPage(int statusCode, const ServerConfig& config){
+}
