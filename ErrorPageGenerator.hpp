383c8050be13bd5863c6ed0112cfb0711f02e6df
mjbalouta <mjbalouta@gmail.com>
2026-03-11 14:11:15 +0000

    fix: removed some files

--- ErrorPageGenerator.hpp
+++
@@ -1,8 +0,0 @@
-#include "Includes.hpp"
-
-class ErrorPageGenerator {
-public:
-    std::string generateErrorPage(int statusCode, const std::string& message);
-    std::string loadCustomErrorPage(int statusCode, const ServerConfig& config);
-    std::string getReasonPhrase(int status_code);
-};
